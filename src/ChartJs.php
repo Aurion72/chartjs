ff5ff7632a12418f9b2f0528e2f7ae5fcd4e2c26
Aurion72 <contact@agence-aurion.fr>
2020-04-06 17:06:29 +0200

    Update ChartJs.php

--- src/ChartJs.php
+++ src/ChartJs.php
@@ -1728,8 +1728,27 @@ class ChartJs
 
         $array = $this->convertFunctions($array);
 
-        $array = array_merge_recursive_distinct($array, $this->raw_config);
+        $array = $this->array_merge_recursive_distinct($array, $this->raw_config);
 
         return $array;
+    }
+    
+    private function array_merge_recursive_distinct ( array &$array1, array &$array2 )
+    {
+        $merged = $array1;
+
+        foreach ( $array2 as $key => &$value )
+        {
+         if ( is_array ( $value ) && isset ( $merged [$key] ) && is_array ( $merged [$key] ) )
+            {
+             $merged [$key] = $this->array_merge_recursive_distinct ( $merged [$key], $value );
+             }
+            else
+             {
+                $merged [$key] = $value;
+             }
+        }
+
+     return $merged;
     }
 }
